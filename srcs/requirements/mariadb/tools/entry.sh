90f2b057331811339951beb3a6be86f4c117d8d6
Pyt45 <hossamaqlzim107@gmail.com>
2021-10-12 18:36:19 +0100

    Done

--- srcs/requirements/mariadb/tools/entry.sh
+++ srcs/requirements/mariadb/tools/entry.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 if [ ! -f "/var/lib/mysql/ib_buffer_pool" ]; then
-# rm /var/lib/mysql/mysql.sock
     /etc/init.d/mariadb setup
     rc-service mariadb start
-    echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql -u root
+    echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'" | mysql -u root
     echo "CREATE DATABASE wordpress;" | mysql -u root
-    echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSOWRD}';" | mysql -u root
+    echo "GRANT ALL PRIVILEGES ON * . * TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'" | mysql -u root
+    echo "FLUSH PRIVILEGES" | mysql -u root
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" | mysql -u root
+    #echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '123321'" | mysql -u root
     echo "FLUSH PRIVILEGES;" | mysql -u root
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSOWRD}';" | mysql -u root;
-    
 fi
 # sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/my.cnf
 # sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
